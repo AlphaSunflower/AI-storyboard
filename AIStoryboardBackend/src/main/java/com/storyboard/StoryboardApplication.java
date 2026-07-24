e4760d7f71c100108be9651a84a48296fe56c726
weichenyi <771038325@qq.com>
2026-07-24 12:24:50 +0800

    chore: init Spring Boot 4 project with dependencies

---
+++ AIStoryboardBackend/src/main/java/com/storyboard/StoryboardApplication.java
@@ -0,0 +1,11 @@
+package com.storyboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StoryboardApplication.class, args);
+    }
+}
